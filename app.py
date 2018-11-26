2e083dd167a8592b61837318a3edf5b4835e30f9
Morampudi Arun <mormapudiarun@gmail.com>
2018-11-26 10:12:33 -0500

    moved fn to services

--- app.py
+++ app.py
@@ -5,46 +5,18 @@ import urllib.request
 import os
 app = Flask(__name__)
 
-port = int(os.getenv("PORT"))
-from services import returning
+# port = int(os.getenv("PORT"))
+from services import returning, function_for_places, main_function
+
 
 @app.route('/')
 def welcome():
     return render_template("home.html")
 
+
 @app.route('/v1/weather')
 def analyze():
-    lat = request.args.get('lat')
-    lng= request.args.get('lng')
-    placename = request.args.get('placename')
-    state = request.args.get('state')
-    city = request.args.get('city')
-    countrycode = request.args.get('countrycode')
-    if placename:
-        placename = placename.replace(" ","%20")
-    zipcode = request.args.get('zipcode')
-    if zipcode:
-        response = urllib.request.urlopen(
-            'http://api.geonames.org/postalCodeSearchJSON?postalcode={}&maxRows=1&username=morampudiarun'.format(
-                zipcode)).read()
-        return returning(response), 200
-    elif lat or lng:
-        response = urllib.request.urlopen(
-            'http://api.geonames.org/findNearbyPostalCodesJSON?lat={}&lng={}&maxRows=1&username=morampudiarun'.format(
-                lat, lng)).read()
-        if lat:
-            return returning(response, lat), 200
-        elif lng:
-            return returning(response, lng), 200
-        else:
-            return returning(response, lat, lng), 200
-    elif placename or city or countrycode or state:
-        response = urllib.request.urlopen(
-            'http://api.geonames.org/findNearbyPostalCodesJSON?placename={}&maxRows=1&username=morampudiarun'.format(
-                placename)).read()
-        return returning(response), 200
-    else:
-        return ({"Error":"Please pass proper Query parameters"}), 400
+    return main_function()
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', debug=True, port= port)
+    app.run(debug=True)
--- services.py
+++ services.py
@@ -1,8 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from geopy import geocoders
 import json
 import urllib.request
+import os
 
+def function_for_places(placename):
+    response = urllib.request.urlopen(
+        'http://api.geonames.org/findNearbyPostalCodesJSON?placename={}&maxRows=1&username=morampudiarun'.format(
+            placename)).read()
+    return returning(response), 200
 
 def returning(response, lat=0, lng=0):
     res = json.loads(response.decode('utf8').replace("'", '"'))
@@ -29,3 +35,41 @@ def returning(response, lat=0, lng=0):
             return jsonify(res2)
         except:
             return ("Invalid input"), 400
+
+
+def main_function():
+    lat = request.args.get('lat')
+    lng= request.args.get('lng')
+    placename = request.args.get('placename')
+    state = request.args.get('state')
+    city = request.args.get('city')
+    countrycode = request.args.get('countrycode')
+    if placename:
+        placename = placename.replace(" ","%20")
+    zipcode = request.args.get('zipcode')
+    if zipcode:
+        response = urllib.request.urlopen(
+            'http://api.geonames.org/postalCodeSearchJSON?postalcode={}&maxRows=1&username=morampudiarun'.format(
+                zipcode)).read()
+        return returning(response), 200
+    elif lat or lng:
+        response = urllib.request.urlopen(
+            'http://api.geonames.org/findNearbyPostalCodesJSON?lat={}&lng={}&maxRows=1&username=morampudiarun'.format(
+                lat, lng)).read()
+        if lat:
+            return returning(response, lat), 200
+        elif lng:
+            return returning(response, lng), 200
+        else:
+            return returning(response, lat, lng), 200
+    elif placename or city or countrycode or state:
+        if placename:
+            return function_for_places(placename)
+        elif city:
+            return function_for_places(city)
+        elif state:
+            return function_for_places(state)
+        elif countrycode:
+            return function_for_places(countrycode)
+    else:
+        return ({"Error":"Please pass proper Query parameters"}), 400
